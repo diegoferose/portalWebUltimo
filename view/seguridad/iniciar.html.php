a8890f6a298a714656b7e8915a8177e9303c19de
David <David@Davidim>
2015-12-03 06:56:17 -0500

    Cerrar sesion funcional

--- view/seguridad/iniciar.html.php
+++ view/seguridad/iniciar.html.php
@@ -2,41 +2,41 @@
 
 <div class="container">
 
-    <div class="row" style="margin-top:20px">
-        <div class="col-xs-12 col-sm-8 col-md-6 col-sm-offset-2 col-md-offset-3">
-            <form role="form" method="post" action="<?php echo $fsConfig->getUrl() . 'index.php/seguridad/login' ?>">
-                <fieldset>
-                    <h2>Registrate</h2>
-                    <hr class="colorgraph">
-                    <div class="form-group">
-                        <input type="email" name="email" id="email" class="form-control input-lg" placeholder="Ingresa Email">
-                    </div>
-                    <div class="form-group">
-                        <input type="password" name="password" id="password" class="form-control input-lg" placeholder="Contraseña">
-                    </div>
-                    <span class="button-checkbox">
-                        <label>Recuerdame</label>
-                        <input type="checkbox" name="remember_me" id="remember_me" checked="checked">
-                        <a href="" class="btn btn-link pull-right">Olvidaste Tu Contraseña?</a>
-                    </span>
-                    <hr class="colorgraph">
-                    <div class="row">
-                        <div class="col-xs-6 col-sm-6 col-md-6">
-                            <input type="submit" class="btn btn-lg btn-success btn-block" value="Iniciar Sesion">
-                        </div>
-                        <div class="col-xs-6 col-sm-6 col-md-6">
-                            <a href="" class="btn btn-lg btn-primary btn-block">Registrar</a>
-                        </div>
-                    </div>
-                </fieldset>
-                <?php if (isset($_SESSION['registroSuccess']) === true): ?>
-                    <br>
-                    <div class="alert alert-success" role="alert"><?php echo $_SESSION['registroSuccess'] ?></div>
-                    <?php unset($_SESSION['registroSuccess']) ?>
-                <?php endif; ?>
-            </form>
-        </div>
+  <div class="row" style="margin-top:20px">
+    <div class="col-xs-12 col-sm-8 col-md-6 col-sm-offset-2 col-md-offset-3">
+      <form role="form" method="post" action="<?php echo $fsConfig->getUrl() . 'index.php/seguridad/login' ?>">
+        <fieldset>
+          <h2>Inicia Sesion</h2>
+          <hr class="colorgraph">
+          <div class="form-group">
+            <input type="text" name="email" id="email" class="form-control input-lg" placeholder="Ingresa Email">
+          </div>
+          <div class="form-group">
+            <input type="password" name="password" id="password" class="form-control input-lg" placeholder="Contraseña">
+          </div>
+          <span class="button-checkbox">
+            <label>Recuerdame</label>
+            <input type="checkbox" name="remember_me" id="remember_me" checked="checked">
+            <a href="" class="btn btn-link pull-right">Olvidaste Tu Contraseña?</a>
+          </span>
+          <hr class="colorgraph">
+          <div class="row">
+            <div class="col-xs-6 col-sm-6 col-md-6">
+              <input type="submit" class="btn btn-lg btn-success btn-block" value="Iniciar Sesion">
+            </div>
+            <div class="col-xs-6 col-sm-6 col-md-6">
+              <a href="<?php echo $fsConfig->getUrl() ?>index.php/portal/nuevo" class="btn btn-lg btn-primary btn-block">Registrar</a>
+            </div>
+          </div>
+        </fieldset>
+        <?php if (isset($_SESSION['registroSuccess']) === true): ?>
+          <br>
+          <div class="alert alert-success" role="alert"><?php echo $_SESSION['registroSuccess'] ?></div>
+          <?php unset($_SESSION['registroSuccess']) ?>
+        <?php endif; ?>
+      </form>
     </div>
+  </div>
 
 </div>
 
